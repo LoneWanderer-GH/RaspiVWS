aa09558865db83d5a0c7a490d6e23566bac8c356
LoneWanderer-GH <LoneWanderer-GH@users.noreply.github.com>
2018-09-01 15:22:23 +0200

    Update Raspi_VLC_Webcam_Stream.sh
    Upgrade script:
    - with better device existence test
    - ssmtp command existence test
    - use some variables and constants for reading / evols

--- Raspi_VLC_Webcam_Stream.sh
+++ Raspi_VLC_Webcam_Stream.sh
@@ -1,31 +1,85 @@
 #!/bin/bash
 #
+
+#CONSTANTS
+readonly K_LED_OFF=0
+readonly K_LED_ON=1
+readonly K_VLC_PARAM_INFINITE_LOOP_ACTIVATED=--loop
+readonly K_VLC_PARAM_INFINITE_LOOP_DEACTIVATED=
+
 # OPTIONS
+WIDTH=1920
+HEIGHT=1080
+LED_COMMAND=K_LED_OFF
 # adapt to video device name
-deviceNb=0
+VIDEO_DEVICE_NB=0
 # loop duration in sec
-fileduration=120
+EACH_MOVIE_DURATION_SEC=120
 #command timeout in sec
-commandtimeout=10800
-#infinite recording
-#loopOption=
-loopOption=--loop
+GLOBAL_RECORD_TIMEOUT=10800
 
-echo -e "Starting VLC stream and record\nDate: `date`\nDeviceNb: ${deviceNb}\nDuration: ${commandtimeout}\nloopOption: ${loopOption}\nFile duration: ${fileduration}" | ssmtp -vvv marc.daconceicao@gmail.com
 
-# WEBCAM CONFIG
-#
-# force video format + led off
-#
-v4l2-ctl -d${deviceNb} --set-fmt-video=width=1920,height=1080,pixelformat=1 --set-ctrl=led1_mode=0
+# destination folder (full path)
+MOVIES_FOLDER=/home/pi/Webcam_Record
 
-# DO THE VLC STREAMING
-##
-# pulseaudio workaround
-timeout ${commandtimeout}s \
-cvlc --alsa-audio-device default \
---sout-file-format \
---run-time=${fileduration} ${loopOption} \
-v4l2:///dev/video${deviceNb}:chroma=h264 :input-slave=alsa://hw:1,0 \
---sout \
-'#transcode{acodec=mpga,ab=128,channels=2,samplerate=44100,threads=4,audio-sync=1}:duplicate{dst=standard{access=file,mux=mp4,dst='/home/pi/Webcam_Record/%Y-%m-%d_%Hh%Mm%Ss_Webcam_Spy.mp4'}:dst=standard{access=http,mux=ts,mime=video/ts,dst=:8099}'
+#http port
+HTTP_PORT=8099
+
+
+#ERROR CODES
+readonly ERROR_MAIL=99
+readonly ERROR_WEBCAM_DOES_NOT_EXIST=1
+
+WEBCAM_DEVICE=/dev/video${VIDEO_DEVICE_NB}
+
+if [ -f ${WEBCAM_DEVICE} ] ; then
+	VLC_C920_STREAM ${VIDEO_DEVICE_NB} ${HTTP_PORT} ${MOVIES_FOLDER}
+else
+	echo
+	echo >&2 "Device ${WEBCAM_DEVICE} required but could not be found.  Aborting.";
+	echo
+	exit ${ERROR_WEBCAM_DOES_NOT_EXIST};
+fi
+
+
+function VLC_C920_STREAM {
+	VIDEO_DEVICE_NB=$1
+	HTTP_PORT=$2
+	MOVIES_FOLDER=$3
+	
+	FILE_NAME_PATTERN=%Y-%m-%d_%Hh%Mm%Ss_Webcam_Spy.mp4
+	VLC_FILE_DUPLICATE_ARG=standard{access=file,mux=mp4,dst='${MOVIES_FOLDER}/${FILE_NAME_PATTERN}'}
+	VLC_HTTP_DUPLICATE_ARG=standard{access=http,mux=ts,mime=video/ts,dst=:${HTTP_PORT}}
+	WEBCAM_DEVICE=/dev/video${VIDEO_DEVICE_NB}
+	MAIL_CMD=ssmtp
+	#infinite recording
+
+	VLC_PARAM_INFINITE_LOOP=${K_VLC_PARAM_INFINITE_LOOP_ACTIVATED}
+	
+	checkSSMTP
+	
+	echo -e "Starting VLC stream and record\nDate: `date`\nVIDEO_DEVICE_NB: ${VIDEO_DEVICE_NB}\nDuration: ${GLOBAL_RECORD_TIMEOUT}\nVLC_PARAM_INFINITE_LOOP: ${VLC_PARAM_INFINITE_LOOP}\nFile duration: ${EACH_MOVIE_DURATION_SEC}" | ssmtp -vvv marc.daconceicao@gmail.com
+
+	# WEBCAM CONFIG
+	#
+	# force video format + led off
+	#
+	v4l2-ctl -d${VIDEO_DEVICE_NB} --set-fmt-video=width=${WIDTH},height=${HEIGHT},pixelformat=1 --set-ctrl=led1_mode=${LED_COMMAND}
+
+	# DO THE VLC STREAMING
+	##
+	# pulseaudio workaround
+	timeout ${GLOBAL_RECORD_TIMEOUT}s \
+	cvlc --alsa-audio-device default \
+	--sout-file-format \
+	--run-time=${EACH_MOVIE_DURATION_SEC}\
+	${VLC_PARAM_INFINITE_LOOP} \
+	v4l2:///dev/video${VIDEO_DEVICE_NB}:chroma=h264 :input-slave=alsa://hw:1,0 \
+	--sout \
+	'#transcode{acodec=mpga,ab=128,channels=2,samplerate=44100,threads=4,audio-sync=1}:duplicate{dst=${VLC_FILE_DUPLICATE_ARG}:dst=${VLC_HTTP_DUPLICATE_ARG}}'
+}
+
+function checkSSMTP {
+	# send mail if ssmtp exists
+	command -v ${MAIL_CMD} >/dev/null 2>&1 || { echo >&2 "I require ${MAIL_CMD} but it's not installed.  Aborting."; exit ${ERROR_MAIL}; }
+}
